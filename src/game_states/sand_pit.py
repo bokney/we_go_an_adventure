bb5125bd156f935a317be5dd1d97aff553eaccc6
bokney <toknyk@gmail.com>
2024-07-25 22:12:54 +0100

    shaping up

--- src/game_states/sand_pit.py
+++ src/game_states/sand_pit.py
@@ -1,20 +1,29 @@
 from sprint import sprint, wait, clear
 
+
 class Actor:
 
-    def __init__(self) -> None:
-        self._x = 0
-        self._y = 0
+    def __init__(self,
+                 name: str,
+                 char: str = 'ö',
+                 x: int = 0,
+                 y: int = 0
+                 ) -> None:
+        self.name = name
+        self.x = x
+        self.y = y
+        self.char = char
         self._visible = True
-        self._
+        # self._
         
-    def place(self, x, y):
+    def place(self, x: int, y: int):
         self.x = x
         self.y = y
 
+
 class Canvas:
 
-    def __init__(self, width, height) -> None:
+    def __init__(self, width: int, height: int) -> None:
         self._width = width
         self._height = height
         self._store = []
@@ -29,16 +38,21 @@ class Canvas:
         for y in range(self._height):
             self._store.append([])
             for x in range(self._width):
+                # self._store[y].append("-")
                 if y == 0 or y == self._height - 1 or x == 0 or x == self._width - 1:
                     self._store[y].append('#')
                 else:
                     self._store[y].append(' ')
+        for a in self._actors:
+            self._store[a.y][a.x] = a.char
 
     def draw(self):
+        self._populate()
         for y in range(self._height):
             print("".join(self._store[y]))
 
 
+# # # # # # # # # # # # # # # # # # # # # # # # 
 
 def obj_place():
     # for y in range(obj_y, obj_y + obj_height):
@@ -50,15 +64,19 @@ def obj_place():
 
 canvas = Canvas(32, 24)
 
-
 def init_sand_pit():
+    canvas.add_actor(Actor("actor_01", "P", 4, 4))
     clear()
     canvas.draw()
 
 
 def loop_sand_pit():
-    
+    result = input(">")
+    canvas.add_actor(Actor("000", result, 8, 8))
+    clear()
+    canvas.draw()
     return 0
 
 def exit_sand_pit():
+    print('Goodbye')
     pass
